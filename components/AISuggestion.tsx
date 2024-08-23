e430ce5e14766c9111279bbbef6138cf6124e8e3
Mohit Tiwari <mmjsmohit@gmail.com>
2024-08-23 11:49:32 +0530

    chore: Add marked library for markdown rendering

--- components/AISuggestion.tsx
+++ components/AISuggestion.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import useSWR from "swr";
+import {marked} from "marked";
 
 const fetcher = (term: string) =>
   fetch("/api/suggestions?term=" + term).then((res) => res.json());
@@ -15,17 +16,19 @@ function AISuggestion({ term }: { term: string }) {
     }
   );
 
+  
   const generateText = () => {
     if (isLoading || isValidating)
       return (
-        <>
+    <>
           <div className="animate-spin rounded-full h-10 w-10 border-b-2 border-white" />
           <p className="text-sm text-gray-400">AI Assistant is thinking...</p>
         </>
       );
-
-    if (error) return <>Error...</>;
-    if (!data) return <>No data</>;
+      
+      if (error) return <>Error...</>;
+      if (!data) return <>No data</>;
+      const htmlContent = marked.parse(data.message);
 
     return (
       <>
@@ -35,7 +38,7 @@ function AISuggestion({ term }: { term: string }) {
           <p className="text-sm text-gray-400">
             AI (Azure Functions) Assistant Suggests:{" "}
           </p>
-          <p className="italic text-xl" dangerouslySetInnerHTML={{ __html: data.message.replace(/\n/g, '<br />') }}></p>
+          <p className="italic text-xl " dangerouslySetInnerHTML={{ __html: htmlContent}}></p>
         </div>
       </>
     );
